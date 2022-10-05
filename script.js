496a7386500c574f3163a1799d71ea265ad8bfab
Brandon U <unzueta.brandon@gmai.com>
2022-10-04 19:59:38 -0700

    added css for slides and beginning javascript logic

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSlides(num) {
+  let i;
+  let slides = document.getElementsByClassName("gallery-section");
+  let dots = document.getElementsByClassName("dot");
+  // CONTINUE WORKING ON Javascript
+}
