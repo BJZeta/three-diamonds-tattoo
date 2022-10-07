e803aee5b11575683ab7c5298374ea85cb330bcb
Brandon U <unzueta.brandon@gmai.com>
2022-10-06 20:36:36 -0700

    js logic for image slides complete

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function addSlides(num) {
+  showSlides((slideIndex += num));
+}
+
+function currentSlide(num) {
+  showSlides((slideIndex = num));
+}
+
 function showSlides(num) {
   let i;
-  let slides = document.getElementsByClassName("gallery-section");
+  let slides = document.getElementsByClassName("slides");
   let dots = document.getElementsByClassName("dot");
-  // CONTINUE WORKING ON Javascript
+  if (num > slides.length) {
+    slideIndex = 1;
+  }
+  if (num < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
